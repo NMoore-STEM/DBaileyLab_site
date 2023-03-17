5c8874fc61fe46b61adf614b082ec53c6d856a85
NMoore-STEM <66744927+NMoore-STEM@users.noreply.github.com>
2023-03-17 16:47:00 -0600

    Bailey Lab Page - Script Clean Up Removed unused function(s). Added some notation. Created new variables to reduce character count.

--- accordians_01.js
+++ accordians_01.js
@@ -1,93 +1,74 @@
+// Bailey Lab Website Script
 $(document).ready(function() {
 //top three list items from #news section (cloned)//
     $(function() {
         var cloneFunction = $("#topNews").clone();
         $("#topNewsClone").html(cloneFunction);
     });
-  
 //accordian behavior//
-$(document).ready(function () {
-    $('.collapseBody').hide();
-    // $('.collapseButton').click(function () {
-    $('.collapseButton').on('click', function () {
-        $('rect.vert').toggle();
-        $('.collapseBody').slideToggle("slow");
-    }),
-    $('.collapseBody2').hide();
-    // $('.collapseButton2').click(function() {
-    $('.collapseButton2').on('click', function() {
-        $('rect.vert2').toggle();
-        $('.collapseBody2').slideToggle("fast");
+    $(function () {
+        $('.collapseBody').hide();
+        $('.collapseButton').on('click', function () {
+            $('rect.vert').toggle();
+            $('.collapseBody').slideToggle("slow");
+        }),
+        $('.collapseBody2').hide();
+        $('.collapseButton2').on('click', function() {
+            $('rect.vert2').toggle();
+            $('.collapseBody2').slideToggle("fast");
+        });
     });
-});
 
 //To Top button on full publication list page//
-$(document).ready(function(){
-    // $(document).scroll(function(){
-    $(document).on('scroll', function(){
-        if ($(document).scrollTop() > 450) {
-            $('#buttonToTop').addClass('reveal');
-        } else {
-            $('#buttonToTop').removeClass('reveal');
-        }
+    var bTop = $('#buttonToTop')
+    $(function(){
+        $(document).on('scroll', function(){
+            if ($(document).scrollTop() > 450) {
+                bTop.addClass('reveal');
+            } else {
+                bTop.removeClass('reveal');
+            }
+        });
+        //Click event to scroll to top
+        bTop.on('click', function(){
+            $('html, body').animate({scrollTop : 0},250);
+            return false;
+        });
+
     });
-
-    //Click event to scroll to top
-    $('#buttonToTop').on('click', function(){
-        $('html, body').animate({scrollTop : 0},250);
-        return false;
-    });
-
-});
-
-//Attempt to return page to top on page refresh
-function applyBeforeunload() {
-    $(window).on('beforeunload', function() {
-      $('body').hide();
-      $(window).scrollTop(0);
-      console.log('applyBeforeunload ran')
-    });
-  }
-  applyBeforeunload();
-  $(document).on('click', '[samepage]', function() {
-    $(window).off('beforeunload');
-    setTimeout(applyBeforeunload, 1000);
-  });
-
 //----------Carousel code
-var disableLeft = $('#carousel_NavLeft').off('click');
-var disableRight = $('#carousel_NavRight').off('click');
-$(function(){
-    $('#carousel_NavLeft').on('click', function(){
-        if (($('#carouselReel').css('left')) < '0px'){
-            $('#carouselReel').animate({
-                left: '+=685px'
-            }, 200, 'swing');
-        } else {
-            disableLeft;
-        }
+    var disableLeft = $('#carousel_NavLeft').off('click');
+        disableRight = $('#carousel_NavRight').off('click');
+        cReel = $('#carouselReel')
+    $(function(){
+        $('#carousel_NavLeft').on('click', function(){
+            if ((cReel.css('left')) < '0px'){
+                cReel.animate({
+                    left: '+=685px'
+                }, 200, 'swing');
+            } else {
+                disableLeft;
+            }
+        });
+        $('#carousel_NavRight').on('click', function(){
+            if ((cReel.css('left')) > '-3900px'){
+                cReel.animate({
+                    left: '-=685px'
+                }, 200, 'swing');
+            } else {
+                disableRight;
+            }
+        })
     });
-    $('#carousel_NavRight').on('click', function(){
-        if (($('#carouselReel').css('left')) > '-3900px'){
-            $('#carouselReel').animate({
-                left: '-=685px'
-            }, 200, 'swing');
-        } else {
-            disableRight;
-        }
-    })
-}
-);
-
 //-----MOBILE menu button----//
-$(document).ready(function(){
-    $('#mobileMenu').on('click', function(){
-        $('.menuClosed').toggleClass('menuOpen');
-        $('#mobileMenu').toggleClass('menuButtonOpen');
+    $(function(){
+        $('#mobileMenu').on('click', function(){
+            $('.menuClosed').toggleClass('menuOpen');
+            $('#mobileMenu').toggleClass('menuButtonOpen');
+        });
+        $('.button').on('click', function(){
+            $('.menuClosed').removeClass('menuOpen');
+            $('#mobileMenu').removeClass('menuButtonOpen');
+        })
     });
-    $('.button').on('click', function(){
-        $('.menuClosed').removeClass('menuOpen');
-        $('#mobileMenu').removeClass('menuButtonOpen');
-    })
-});
 })
